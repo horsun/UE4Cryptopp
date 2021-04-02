97b2c945a57ec75ccbff08e6a86f149edc7b82ca
jinhao <im7h1995@hotmail.com>
2021-04-02 19:47:12 +0800

    添加lib

--- Source/TBBCrypt/Private/TBBCryptBPLibrary.cpp
+++ Source/TBBCrypt/Private/TBBCryptBPLibrary.cpp
@@ -7,65 +7,71 @@
 #include "modes.h"	     // CFB_Mode  
 #include <iostream>   // std:cerr    
 #include <sstream>   // std::stringstream    
-#include <string>  
+#include <string>
 #include "channels.h"
 #include "mqueue.h"
 #include "misc/Base64.h"
 using namespace std;
 using namespace CryptoPP;
-#pragma comment(lib, "cryptlib.lib" )  
+#pragma comment(lib, "cryptlib.lib" )
 
 UTBBCryptBPLibrary::UTBBCryptBPLibrary(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-
 }
-//reinterpret_cast<const unsigned char*> (ciphertextDecode.c_str()
-static  std::string KeyStr = "0123456789ABCDEF0123456789ABCDEF";
+
+static std::string KeyStr = "0123456789ABCDEF0123456789ABCDEF";
 static const FString IVStr = "ABCDEF0123456789";
+
 SecByteBlock getKey()
 {
 	SecByteBlock key(AES::MAX_KEYLENGTH);
 	memset(key, 0x30, key.size());
-	KeyStr.size() <= AES::MAX_KEYLENGTH ? memcpy(key, KeyStr.c_str(), KeyStr.size()) : memcpy(key, KeyStr.c_str(), AES::MAX_KEYLENGTH);
+	KeyStr.size() <= AES::MAX_KEYLENGTH
+		? memcpy(key, KeyStr.c_str(), KeyStr.size())
+		: memcpy(key, KeyStr.c_str(), AES::MAX_KEYLENGTH);
 	return key;
 }
+
 FString EnCrypto(string inPlain)
 {
 	string cipher;
 	string cipherOut;
 	SecByteBlock Key = getKey();
-	ECB_Mode< AES >::Encryption e;
-	e.SetKey((byte *)Key, AES::MAX_KEYLENGTH);
+	ECB_Mode<AES>::Encryption e;
+	e.SetKey((byte*)Key, AES::MAX_KEYLENGTH);
 
 	StringSource(inPlain, true,
-		new StreamTransformationFilter(e,
-			new StringSink(cipher)
-			,BlockPaddingSchemeDef::ONE_AND_ZEROS_PADDING
-			,true
-		) //这里开始设置加密的明文和秘钥     
+	             new StreamTransformationFilter(e,
+	                                            new StringSink(cipher)
+	                                            , BlockPaddingSchemeDef::ONE_AND_ZEROS_PADDING
+	                                            , true
+	             ) //这里开始设置加密的明文和秘钥     
 	);
 	StringSource(cipher, true,
-		new HexEncoder(
-			new StringSink(cipherOut)
-		) // HexEncoder
+	             new HexEncoder(
+		             new StringSink(cipherOut)
+	             ) // HexEncoder
 	); // StringSource
 	UE_LOG(LogTemp, Warning, TEXT(" the cipher:%s"), UTF8_TO_TCHAR( cipherOut.c_str()));
 
 	return UTF8_TO_TCHAR(cipherOut.c_str());
 }
+
 FString DeCrypto(string inCipher)
 {
+	string inCipherStr;
+	StringSource(inCipher, true, new HexDecoder(new StringSink(inCipherStr)));
 	string recovered;
-	ECB_Mode< AES >::Decryption d;
+	ECB_Mode<AES>::Decryption d;
 	SecByteBlock Key = getKey();
-	d.SetKey((byte *)Key, AES::MAX_KEYLENGTH);
-	StringSource s(inCipher, true,
-		new StreamTransformationFilter(d,
-			new StringSink(recovered)
-			, BlockPaddingSchemeDef::ONE_AND_ZEROS_PADDING
-			, true
-		) // StreamTransformationFilter
+	d.SetKey((byte*)Key, AES::MAX_KEYLENGTH);
+	StringSource s(inCipherStr, true,
+	               new StreamTransformationFilter(d,
+	                                              new StringSink(recovered)
+	                                              , BlockPaddingSchemeDef::ONE_AND_ZEROS_PADDING
+	                                              , true
+	               ) // StreamTransformationFilter
 	); // StringSource
 	//StringSource(recovered, true,
 	//	new HexEncoder(
@@ -75,12 +81,11 @@ FString DeCrypto(string inCipher)
 
 	UE_LOG(LogTemp, Warning, TEXT(" the recovered:%s"), UTF8_TO_TCHAR(recovered.c_str()));
 	return UTF8_TO_TCHAR(recovered.c_str());
-
 }
 
 FString UTBBCryptBPLibrary::AESFunctionLib(FString inString, ECryptMode mode, ECryActionType action)
 {
-	FString  OutString = "";
+	FString OutString = "";
 	bool test = false;
 	string inString2 = TCHAR_TO_UTF8(*inString);
 	switch (action)
@@ -133,5 +138,4 @@ FString UTBBCryptBPLibrary::AESFunctionLib(FString inString, ECryptMode mode, EC
 		break;
 	}
 	return OutString;
-
 }
